ca800a6c30b616b3493748837702bd3c47dc6543
varuj02 <cool.varuzh@gmil.com>
2018-06-12 17:26:36 +0400

    theEnd

--- public/functions.js
+++ public/functions.js
@@ -2,8 +2,32 @@ function GameOver() {
     background("#acacac")
     fill(0);
     textSize(50);
-    text("GAME OVER", 290, 250);
+    text("GAME OVER", (canvasWidth - 5) * side / 2, (canvasHeight - 1) * side / 2);
 
+    textSize(30);
+    text("Click for new game", (canvasWidth - 5) * side / 2, (canvasHeight + 1) * side / 2);
+
+}
+function New_Game() {
+    bananas = [], hearts = [], coins = []
+    player = { x: 0, y: (canvasHeight - 2) * side, score: 0, life: 5, coin: 0 }
+    interval = 150;// /FPS  sec
+    time = 0; //sec
+    time2 = 0;
+    playerStep = 8, bananaStep = 2, heartStep = 1, coinStep = 3
+    images, level, scoreimg, buy = false, right = true, pause = false, inShop = false, gameover = false;
+    items = [
+        { name: 'monkey', price: 0, buy: "Equiped", image: "images/monkey_R.png", HTML: '' },
+        { name: 'minion', price: 10, buy: "Buy", image: "images/minion_R.png", HTML: '' },
+        { name: 'mario', price: 20, buy: "Buy", image: "images/mario_R.png", HTML: '' },
+        { name: 'sonic', price: 25, buy: "Buy", image: "images/sonic_R.png", HTML: '' },
+        { name: 'batman', price: 40, buy: "Buy", image: "images/batman_R.png", HTML: '' },
+        { name: 'deadpool', price: 50, buy: "Buy", image: "images/deadpool_R.png", HTML: '' },
+        { name: 'extralife', price: 10, buy: "Buy", image: "images/heart.png", HTML: '' }
+    ]
+    lifeElem.innerHTML = "Life: " + player.life;
+    scoreElem.innerHTML = "Score: " + player.score;
+    gameover = false;
 }
 function drawResurces() {
     for (let banana of bananas) {
@@ -41,22 +65,48 @@ function in_Player(bananas) {
 
     }
 }
-function preload(){
-    //    document.body.onload = addElement;
-    
-    //    function addElement() {
-        
-        //        // create a new div element 
-        //        var newP = document.createElement("p");
-        //        var footer = document.createElement("footer");
-        //        // and give it some content 
-        //        var newContent = document.createTextNode("By Varujan Margaryan");
-        //        // add the text node to the newly created div
-        //        newP.appendChild(newContent);
-        //        footer.appendChild(newP);
-        
-        //        // add the newly created element and its content into the DOM 
-        //        canvas.parentNode.insertBefore(footer, canvas.nextSibling);
-        
-        //    }
+function Time() {
+    //frameRate(FPS)
+    playerStep *= 1.0001;
+    bananaStep *= 1.0001;
+    heartStep *= 1.0001;
+    coinStep *= 1.0001;
+    time++;
+    //FPS += 0.01
+    interval -= 0.01;
+    if (time >= interval) {
+        var randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
+        bananas.push({ x: randX, y: 0 });
+        time = 0;
+        time2++;
+        if (Math.random() > 0.6) {
+            var randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
+            coins.push({ x: randX, y: 0 });
+        }
+    }
+    if (time2 >= 20) {
+        let randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
+        hearts.push({ x: randX, y: 0 });
+        time2 = 0;
+    }
+}
+function Pause_Game() {
+    for (let banana of bananas) {
+        let x = banana.x;
+        let y = banana.y;
+        banana.y += bananaStep;
+    }
+    for (let heart of hearts) {
+        let x = heart.x;
+        let y = heart.y;
+        heart.y += heartStep;
+    }
+    for (let coin of coins) {
+        let x = coin.x;
+        let y = coin.y;
+        coin.y += coinStep;
+    }
+    fill(0);
+    textSize(50);
+    text("PAUSE", (canvasWidth - 5) * side / 2, (canvasHeight - 1) * side / 2);
 }
--- public/script.js
+++ public/script.js
@@ -1,81 +1,24 @@
 var canvasHeight = 10;
-var canvasWidth = 18;
+var canvasWidth = 25;
 var side = 50;
 
 var bananas = [], hearts = [], coins = []
 var player = { x: 0, y: (canvasHeight - 2) * side, score: 0, life: 5, coin: 0 }
-var interval = 100;// /FPS  sec
-var time = 1000; //sec
+var interval = 150;// /FPS  sec
+var time = 0; //sec
 var time2 = 0;
-var FPS = 30;
-var playerStep = 6, bananaStep = 2, heartStep = 1, coinStep = 3
-var images, level, scoreimg, buy = false, right = true, pause = false
+var playerStep = 8, bananaStep = 2, heartStep = 1, coinStep = 3
+var images, level, scoreimg, buy = false, right = true, pause = false, inShop = false,gameover=false;
 var items = [
-    { name: 'monkey', price: 0, buy: "Equiped", HTML: '' },
-    { name: 'minion', price: 1, buy: "Buy", HTML: '' },
-    { name: 'mario', price: 2, buy: "Buy", HTML: '' },
-    { name: 'batman', price: 5, buy: "Buy", HTML: '' }
+    { name: 'monkey', price: 0, buy: "Equiped", image: "images/monkey_R.png", HTML: '' },
+    { name: 'minion', price: 10, buy: "Buy", image: "images/minion_R.png", HTML: '' },
+    { name: 'mario', price: 20, buy: "Buy", image: "images/mario_R.png", HTML: '' },
+    { name: 'sonic', price: 25, buy: "Buy", image: "images/sonic_R.png", HTML: '' },
+    { name: 'batman', price: 40, buy: "Buy", image: "images/batman_R.png", HTML: '' },
+    { name: 'deadpool', price: 50, buy: "Buy", image: "images/deadpool_R.png", HTML: '' },
+    { name: 'extralife', price: 10, buy: "Buy", image: "images/heart.png", HTML: '' }
+
 ]
-var mainDiv = document.getElementById("main");
-
-var scoreElem = document.getElementById("score");
-scoreElem.innerText = "Score: 0";
-
-var lifeElem = document.getElementById("life");
-lifeElem.innerText = "Life: 5"
-
-var coinElem = document.getElementById("coin");
-coinElem.innerText = "Coins: 0";
-
-var pauseimg = document.createElement('img');
-pauseimg.setAttribute("src", "images/pause.png");
-pauseimg.id = "pause";
-mainDiv.appendChild(pauseimg);
-pauseimg.onclick = function () {
-    if (pause) pause = false
-    else pause = true;
-}
-
-var shopDiv = document.getElementById("shop");
-
-for (var item of items) {
-    var itemDiv = document.createElement('div');
-    itemDiv.className = "item";
-
-    var itemH2 = document.createElement('h2');
-    itemH2.className = 'itemName';
-    itemH2.innerHTML = item.name;
-
-    var itemImg = document.createElement("img");
-    itemImg.setAttribute("src", "images/" + item.name + "_R.png");
-
-    var itemPrice = document.createElement("snap");
-    itemPrice.className = "price";
-    itemPrice.innerHTML = "Price: " + item.price;
-
-    var itemBuy = document.createElement("snap");
-    itemBuy.className = "buy";
-    itemBuy.innerHTML = item.buy;
-
-    if (item.name == "minion") {
-        var minion = itemBuy;
-    }
-    else if (item.name == "monkey") {
-        var monkey = itemBuy;
-    }
-    else if (item.name == "mario") {
-        var mario = itemBuy;
-    }
-    else if (item.name == "batman") {
-        var batman = itemBuy;
-    }
-    itemDiv.appendChild(itemH2)
-    itemDiv.appendChild(itemImg);
-    itemDiv.appendChild(itemPrice);
-    itemDiv.appendChild(itemBuy);
-    shopDiv.appendChild(itemDiv);
-    item.HTML = { h2: itemH2, img: itemImg, price: itemPrice, buy: itemBuy, div: itemDiv }
-}
 
 // 
 // function preLoad(){
@@ -92,12 +35,18 @@ function setup() {
         minion_R: loadImage('images/minion_R.png'),
         mario_L: loadImage('images/mario_L.png'),
         mario_R: loadImage('images/mario_R.png'),
+        sonic_L: loadImage('images/sonic_L.png'),
+        sonic_R: loadImage('images/sonic_R.png'),
         batman_L: loadImage('images/batman_L.png'),
         batman_R: loadImage('images/batman_R.png'),
+        deadpool_L: loadImage('images/deadpool_L.png'),
+        deadpool_R: loadImage('images/deadpool_R.png'),
         monkey_score: loadImage("images/monkey_score.png"),
         minion_score: loadImage('images/minion_score.png'),
         mario_score: loadImage("images/mario_score.png"),
+        sonic_score: loadImage("images/sonic_score.png"),
         batman_score: loadImage("images/batman_score.png"),
+        deadpool_score: loadImage("images/deadpool_score.png"),
         heart: loadImage('images/heart.png'),
         coin: loadImage('images/coin.png')
     };
@@ -115,41 +64,18 @@ function setup() {
 // }
 
 function draw() {
-    if (pause) {
-        fill(0);
-        textSize(50);
-        text("PAUSE", 300, 250);
-        return;
-    }
     if (player.life <= 0) {
+        gameover = true;
+    }
+    if (gameover) {
         GameOver();
         return;
     }
-    //frameRate(FPS)
-    playerStep *= 1.0001;
-    bananaStep *= 1.0001;
-    heartStep *= 1.0001;
-    coinStep *= 1.0001;
-    time++;
-    //FPS += 0.01
-    interval -= 0.01;
-    if (time >= interval) {
-        var randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
-        bananas.push({ x: randX, y: 0 });
-        time = 0;
-        time2++;
-        if (Math.random() > 0.6) {
-            var randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
-            coins.push({ x: randX, y: 0 });
-        }
+    coinElem.innerHTML = "Coins: " + player.coin;
+    if (inShop) {
+         background('lightgray')
+        return
     }
-    if (time2 >= 20) {
-        let randX = Math.floor(Math.random() * (canvasWidth - 1) * side);
-        hearts.push({ x: randX, y: 0 });
-        time2 = 0;
-    }
-
-
     //__________________LEVELS______________
     // if (player.score < 200 && !buy) {//level1
     //     playerimg = { L: images.monkey_L, R: images.monkey_R };
@@ -163,7 +89,6 @@ function draw() {
 
     background("white");
 
-    drawResurces()
     //DRAW PLAYER
     if (right) {
         image(playerimg.R, player.x, player.y, 2 * side, 2 * side);
@@ -171,7 +96,12 @@ function draw() {
     else {
         image(playerimg.L, player.x, player.y, 2 * side, 2 * side);
     }
-
+    if (pause) {
+       Pause_Game()
+        return;
+    }
+    Time();
+    drawResurces()
     for (let i in bananas) {
         let banana = bananas[i];
         let x = banana.x;
@@ -226,9 +156,8 @@ function draw() {
             coins.splice(i, 1);
         }
     }
-    coinElem.innerHTML = "Coins: " + player.coin;
-
-
+    
+    
     if (keyIsDown(RIGHT_ARROW)) {
         if (player.x >= (canvasWidth - 2) * side) return;
         player.x += playerStep;
@@ -246,15 +175,157 @@ function draw() {
 
 
 // function coins(){
-//     playerimg = { L: images.minion_L, R: images.minion_R };
+    //     playerimg = { L: images.minion_L, R: images.minion_R };
+    
+    // }
+    //var minionSnap = document.getElementById('minion');
+    
+    //_____________________________GETING ELEMENTS_________________
+    
+    var headerDiv = document.getElementById("header");
+    
+    var scoreElem = document.getElementById("score");
+    scoreElem.innerText = "Score: 0";
+    
+    var lifeElem = document.getElementById("life");
+    lifeElem.innerText = "Life: 5"
+    
+    var coinElem = document.getElementById("coin");
+    coinElem.innerText = "Coins: 0";
+    
+    
+    var shopDiv = document.getElementById("shop");
+    
+var shopimg = document.createElement('img');
+shopimg.setAttribute("src", "images/shop.png");
+shopimg.id = "shop";
+headerDiv.appendChild(shopimg);
+shopimg.onclick = function () {
+    if (inShop) {
+        inShop = false;
+        shopimg.setAttribute("src", "images/shop.png");
+        shopDiv.setAttribute("style", "z-index:-100;");
+        
+    }
+    else {
+        inShop = true;
+        shopimg.setAttribute("src", "images/home.png");
+        shopDiv.setAttribute("style", "z-index:100;");
+        
+    }
+}
+var pauseimg = document.createElement('img');
+pauseimg.setAttribute("src", "images/pause.png");
+pauseimg.id = "pause";
+headerDiv.appendChild(pauseimg);
+pauseimg.onclick = function () {
+    if (pause) {
+        pause = false;
+        pauseimg.setAttribute("src", "images/pause.png");
+    }
+    else {
+        pause = true;
+        pauseimg.setAttribute("src", "images/play.png");
+    }
+}
 
-// }
-//var minionSnap = document.getElementById('minion');
 
+
+//______________CREATING ITEM ELEMENTS___________
+
+for (var item of items) {
+    var itemDiv = document.createElement('div');
+    itemDiv.className = "item";
+    
+    var itemH2 = document.createElement('h2');
+    itemH2.className = 'itemName';
+    itemH2.innerHTML = item.name;
+    
+    var itemImg = document.createElement("img");
+    itemImg.setAttribute("src",item.image);
+    itemImg.className = 'item_Img';
+    
+    var itemPrice = document.createElement("snap");
+    itemPrice.className = "price";
+    itemPrice.innerHTML = "Price: " + item.price;
+    
+    var itemBuy = document.createElement("snap");
+    itemBuy.className = "buy";
+    itemBuy.innerHTML = item.buy;
+    
+    switch (item.name) {
+        case "monkey": var monkey = itemBuy;itemBuy.style.color='green'; break;
+        case "minion": var minion = itemBuy; break;
+        case "mario": var mario = itemBuy; break;
+        case "sonic": var sonic = itemBuy; break;
+        case "batman": var batman = itemBuy; break;
+        case "deadpool": var deadpool = itemBuy; break;
+        case "extralife": var extralife = itemBuy; break;
+        
+        
+        
+    }
+    // if (item.name == "monkey") {
+        //     var monkey = itemBuy;
+        // }
+        // else if (item.name == "minion") {
+            //     var minion = itemBuy;
+            // }
+        // else if (item.name == "mario") {
+            //     var mario = itemBuy;
+            // }
+            // else if (item.name == "sonic") {
+                //     var sonic = itemBuy;
+                // }
+                // else if (item.name == "batman") {
+                    //     var batman = itemBuy;
+                    // }
+                    // else if (item.name == "deadpool") {
+                        //     var deadpool = itemBuy;
+                        // }
+                        // else if (item.name == "extralife") {
+            //     var extralife = itemBuy;
+            // }
+            itemDiv.appendChild(itemH2)
+            itemDiv.appendChild(itemImg);
+            itemDiv.appendChild(itemPrice);
+            itemDiv.appendChild(itemBuy);
+            shopDiv.appendChild(itemDiv);
+            item.HTML = { h2: itemH2, img: itemImg, price: itemPrice, buy: itemBuy, div: itemDiv }
+}
+
+//footer
+
+document.body.onload = addElement;
+
+function addElement() {
+    var canvasElem = document.getElementById("defaultCanvas0");
+    canvasElem.onclick = function (){
+        if(gameover){
+            New_Game();
+        }
+    }
+
+    // create a new div element 
+    var newP = document.createElement("p");
+    var footer = document.createElement("footer");
+    // and give it some content 
+    var newContent = document.createTextNode("By Varuj & Manch");
+    // add the text node to the newly created div
+    newP.appendChild(newContent);
+    footer.appendChild(newP);
+    
+    // add the newly created element and its content into the DOM 
+    canvas.parentNode.insertBefore(footer, canvas.nextSibling);
+    
+}
+
+
+//___________________________ONCLICK FUNCTIONS________________
 
 monkey.onclick = function () {
     item = items[0]
-
+    
     if (item.buy == "Equip") {
         scoreimg = loadImage("images/" + item.name + "_score.png")
         playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
@@ -286,7 +357,6 @@ monkey.onclick = function () {
         item.HTML.buy.style.color = "green";
     }
 }
-
 minion.onclick = function () {
     item = items[1]
 
@@ -324,7 +394,7 @@ minion.onclick = function () {
 
 mario.onclick = function () {
     item = items[2]
-
+    
     if (item.buy == "Equip") {
         scoreimg = loadImage("images/" + item.name + "_score.png")
         playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
@@ -356,11 +426,43 @@ mario.onclick = function () {
         item.HTML.buy.style.color = "green";
     }
 }
-
-
-batman.onclick = function () {
+sonic.onclick = function () {
     item = items[3]
-
+    
+    if (item.buy == "Equip") {
+        scoreimg = loadImage("images/" + item.name + "_score.png")
+        playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
+        for (let item of items) {
+            if (item.buy == "Equiped") {
+                item.buy = "Equip";
+                item.HTML.buy.innerHTML = item.buy;
+                item.HTML.buy.style.color = "red"
+            }
+        }
+        item.buy = "Equiped";
+        item.HTML.buy.innerText = item.buy;
+        item.HTML.buy.style.color = "green";
+    }
+    else if (player.coin >= item.price && item.buy == "Buy") {
+        scoreimg = loadImage("images/" + item.name + "_score.png")
+        playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
+        for (let item of items) {
+            if (item.buy == "Equiped") {
+                item.buy = "Equip";
+                item.HTML.buy.innerHTML = item.buy;
+                item.HTML.buy.style.color = "red"
+            }
+        }
+        player.coin -= item.price;
+        console.log("equiped")
+        item.buy = "Equiped";
+        item.HTML.buy.innerText = item.buy;
+        item.HTML.buy.style.color = "green";
+    }
+}
+batman.onclick = function () {
+    item = items[4]
+    
     if (item.buy == "Equip") {
         scoreimg = loadImage("images/" + item.name + "_score.png")
         playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
@@ -393,4 +495,50 @@ batman.onclick = function () {
     }
 }
 
+
+deadpool.onclick = function () {
+    item = items[5]
+    
+    if (item.buy == "Equip") {
+        scoreimg = loadImage("images/" + item.name + "_score.png")
+        playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
+        for (let item of items) {
+            if (item.buy == "Equiped") {
+                item.buy = "Equip";
+                item.HTML.buy.innerHTML = item.buy;
+                item.HTML.buy.style.color = "red"
+            }
+        }
+        item.buy = "Equiped";
+        item.HTML.buy.innerText = item.buy;
+        item.HTML.buy.style.color = "green";
+    }
+    else if (player.coin >= item.price && item.buy == "Buy") {
+        scoreimg = loadImage("images/" + item.name + "_score.png")
+        playerimg = { L: loadImage("images/" + item.name + "_L.png"), R: loadImage("images/" + item.name + "_R.png") };
+        for (let item of items) {
+            if (item.buy == "Equiped") {
+                item.buy = "Equip";
+                item.HTML.buy.innerHTML = item.buy;
+                item.HTML.buy.style.color = "red"
+            }
+        }
+        player.coin -= item.price;
+        console.log("equiped")
+        item.buy = "Equiped";
+        item.HTML.buy.innerText = item.buy;
+        item.HTML.buy.style.color = "green";
+    }
+}
+
+extralife.onclick = function () {
+    item = items[6]
+    
+    if (player.coin >= item.price && gameover) {
+        player.life++;
+        player.coin -= item.price;
+        lifeElem.innerHTML = "Life: " + player.life;
+        
+    }
+}
 
